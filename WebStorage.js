7306709af019cf3ff24fa71bb81a87844afe3264
Ganesh8999 <Ganesh8999@github.com>
2021-02-28 06:52:22 +0530

    WebStorage use

---
+++ WebStorage.js
@@ -0,0 +1,7 @@
+// How do you check web storage browser support
+
+if (typeof Storage !== "undefined") {
+  // code for localStorage/sessionStorage
+} else {
+  // No support for the Storage support
+}
