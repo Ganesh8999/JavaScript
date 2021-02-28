036a57751e26100171b2299705ae6713c4c7f3c2
Ganesh8999 <Ganesh8999@github.com>
2021-02-28 10:41:14 +0530

    callback in callback demo code

---
+++ CallbackInCallback.js
@@ -0,0 +1,11 @@
+loadScript("/script1.js", function (script) {
+  console.log("first script is loaded ");
+  loadScript("/script2.js", function (script) {
+    console.log("second script is loaded");
+
+    loadScript("script3.js", function (script) {
+      console.log("third script is loaded");
+      // After all script is loaded.... :)
+    });
+  });
+});
