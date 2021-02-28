70b09f7bd2a20aa6174c97bd74f267bc79774ef8
Ganesh8999 <Ganesh8999@github.com>
2021-02-28 06:51:42 +0530

    how to implement sessionStorge

---
+++ SessionStorage.js
@@ -0,0 +1,12 @@
+// save the data to the session storage
+sessionStorage.set("key", "value");
+
+// get saved data from the sessionStorage
+let data = sessionStorage.getItem("key");
+
+console.log(data);
+// // remove the data from sessionStorage
+// sessionStorage.removeItem("key");
+
+// // remove all data from sessionStorage
+// sessionStorage.clear();
