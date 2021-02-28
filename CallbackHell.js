fd158ba73d77128da8529a5ea7e22d1171790e2c
Ganesh8999 <Ganesh8999@github.com>
2021-02-28 10:24:31 +0530

    callback hell demo

---
+++ CallbackHell.js
@@ -0,0 +1,11 @@
+// Callback hell have multiple nested callbacks
+
+async1(function () {
+  async2(function () {
+    async3(function () {
+      async4(function () {
+        // can be more ..no one knowsa about it,  this is callback hell but don't worry you will meet here Angel :) Lucifer Morningstar
+      });
+    });
+  });
+});
