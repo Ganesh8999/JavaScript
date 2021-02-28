c34944c3430c500bbfceb36abe07c97e6fa7f2ed
Ganesh8999 <Ganesh8999@github.com>
2021-02-28 06:50:48 +0530

    difference between let and war

---
+++ DiffBetLetAndVar.js
@@ -0,0 +1,13 @@
+function checkVarAndLet(username) {
+  if (username) {
+    console.log(salary);
+    //console.log(age);
+    let age = 30;
+    var salary = 67000;
+  }
+
+  console.log(salary);
+  //  console.log(age);
+}
+
+checkVarAndLet({ user: "Ganesh Singh" });
